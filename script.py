9c32b8d7b63f5b048db52ce7b2973a56664d74da
sylvain <slourdenadin@gmail.com>
2018-02-27 16:41:28 +0100

    python

---
+++ script.py
@@ -0,0 +1,89 @@
+#!/usr/bin/pyton.3.6
+
+'''
+texte = "je suis du texte"
+print (texte * 3)
+
+txt= "HEllo! Wolrd!!!"
+
+print (len(txt))
+
+print (txt[1])
+
+print (txt[0:5])
+
+print("bonjour %s je m'appelle " % ("Medhi"))
+
+text = "je suis du \"texte\""
+print(text)
+
+
+maListe=["1er","deuxieme","troisieme"]
+
+secondList = maListe[:]
+
+maListe[0]="toto"
+
+print (secondList)
+
+
+maListe = range(15)
+
+print(maListe)
+
+Liste = list(range(10))
+
+liste.extend(maListe)
+
+print(liste)
+
+
+monTuple=()
+
+print (type(monTuple))
+
+monTuple = ("toto",)
+
+print (type(monTuple))
+
+monTuple[0] = "error"
+
+monDico={}
+monDico["name"]="Medhi"
+monDico["height"]="1m90"
+print (monDico)
+
+
+def ma_function():
+	print("salut les gens")
+
+ma_function()
+
+def ma_function(param):
+	print(param)
+ma_function("hey")
+
+
+def somme(a,b):
+	return a+b 
+
+somme = somme(1,5)
+print (somme)
+
+a= "salut"
+c= 5
+
+def test():
+	b="test"
+	print (c)
+
+test()
+print(a)
+print(b)
+valeur=input("Enter your value")
+
+
+random.choice([1,2,3,4,5])
+'''
+i=1 
+While i < 10:
